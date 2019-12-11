942c858cbaa68260f5d1db3c0add72ee4461821a
Kristina <kristina.tanya@gmail.com>
2019-12-11 15:53:13 -0800

    built board component, rendered list of cards from json file

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,19 +7,35 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-class Board extends Component {
+class Board extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      cards: [],
+      cards: CARD_DATA["cards"],
     };
   }
+  
+  listCards(cards) {
+    const cardElements = cards.map((card, i) => {
+      return (
+        <Card
+          key={i}
+          {...card}
+        />
+      );
+    });
+
+    return cardElements;
+  }
+  
 
   render() {
     return (
       <div>
-        Board
+        <ul>
+          { this.listCards(this.state.cards) }
+        </ul>
       </div>
     )
   }
