d4fcfc725fddf58053b56ddabaea92ba51dfe761
Kristina <kristina.tanya@gmail.com>
2019-12-11 16:09:17 -0800

    card component built and showing cards from json data. wave 1 complete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-class Card extends Component {
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  
   render() {
+    const { text, emoji } = this.props;
+    
     return (
       <div className="card">
-        Card
+        <section className="card__content">
+          <p className="card__content-text">{ text }</p>
+          <p className="card__content-emoji">{ emoji }</p>
+        </section>
       </div>
     )
   }
