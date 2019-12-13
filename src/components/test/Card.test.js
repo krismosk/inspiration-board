78724ce257fc4f20b52617c75100cf7e417b337b
Kristina <kristina.tanya@gmail.com>
2019-12-13 11:32:11 -0800

    board deployed to github pages. wave 3 complete

--- src/components/test/Card.test.js
+++ src/components/test/Card.test.js
@@ -6,6 +6,9 @@ describe('Card', () => {
   test('Snapshot test for Card', () => {
     const container = render(
       <Card
+        key={1}
+        text={"hi"}
+        emoji={"heart_eyes"}
       />);
       
     expect(container.asFragment()).toMatchSnapshot();
