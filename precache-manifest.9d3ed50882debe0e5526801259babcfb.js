ca4ce6e73f0f23a40057d44735d80649fbec9372
Kristina <kristina.tanya@gmail.com>
2019-12-13 11:30:20 -0800

    Updates

---
+++ precache-manifest.9d3ed50882debe0e5526801259babcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8761fcce0843794e74469db4bdd15b8",
+    "url": "/inspiration-board/index.html"
+  },
+  {
+    "revision": "22ae0f455af1eb2e472b",
+    "url": "/inspiration-board/static/css/main.0b570879.chunk.css"
+  },
+  {
+    "revision": "4842f16f5566299ee47c",
+    "url": "/inspiration-board/static/js/2.d0395555.chunk.js"
+  },
+  {
+    "revision": "098d96cd5327dd3fc6707a77e047f86b",
+    "url": "/inspiration-board/static/js/2.d0395555.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22ae0f455af1eb2e472b",
+    "url": "/inspiration-board/static/js/main.66141a43.chunk.js"
+  },
+  {
+    "revision": "7b3d42590fd4530dd51b",
+    "url": "/inspiration-board/static/js/runtime-main.ecf8ceb5.js"
+  }
+]);
